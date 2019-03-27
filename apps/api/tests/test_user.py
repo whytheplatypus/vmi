5264f6eb480d8622ef0c4ff8a93ac46601350fb4
whytheplatypus <whytheplatypus@gmail.com>
2019-03-27 11:47:13 -0400

    Provide RESTful account CRUD

--- apps/api/tests/test_user.py
+++ apps/api/tests/test_user.py
@@ -1,9 +1,12 @@
+import datetime
 from django.test import TestCase
 from rest_framework.test import RequestsClient
 from apps.accounts.models import UserProfile
 
 
 class UserTestCase(TestCase):
+    maxDiff = None
+
     def setUp(self):
         pass
 
@@ -12,7 +15,7 @@ class UserTestCase(TestCase):
         response = client.post(
             "http://testserver/api/v1/user/",
             json={
-                "username": "james",
+                "preferred_username": "james",
                 "given_name": "James",
                 "family_name": "Kirk",
                 "gender": "M",
@@ -23,10 +26,10 @@ class UserTestCase(TestCase):
                 "email": "jamess@example.com",
              })
         self.assertEqual(response.status_code, 201, response.text)
-        self.assertDictEqual(response.json(), {
-            "iss": "https://alpha.verifymyidentity.com",
-            "subject": "123456789012345", 
-            "username": "james",
+        self.assertDictContainsSubset({
+            "iss": "http://localhost",
+            # "subject": "123456789012345",
+            "preferred_username": "james",
             "given_name": "James",
             "family_name": "Kirk",
             "name": "James Kirk",
@@ -35,11 +38,98 @@ class UserTestCase(TestCase):
             "nickname": "Jim",
             "phone_number": "+15182345678",
             "email": "jamess@example.com",
-            "ial": 1,
-            "id_assursance" : [],
-            "document" : [],
+            "ial": '1',
+            # "id_assursance": [],
+            "document": [],
             "address": []
-        })
+        }, response.json())
 
-        up = UserProfile.objects.get(sub=response.json()['subject'])
-        self.assertEqual(up.user.username, "bob")
+        up = UserProfile.objects.get(subject=response.json()['sub'])
+        self.assertEqual(up.user.username, "james")
+
+    def test_read_user_success(self):
+        client = RequestsClient()
+        create_response = client.post(
+            "http://testserver/api/v1/user/",
+            json={
+                "preferred_username": "james",
+                "given_name": "James",
+                "family_name": "Kirk",
+                "gender": "M",
+                "password": "tree garden jump fox",
+                "birthdate": "1952-01-03",
+                "nickname": "Jim",
+                "phone_number": "+15182345678",
+                "email": "jamess@example.com",
+             })
+        response = client.get(
+             "http://testserver/api/v1/user/{}/".format(create_response.json()['sub']))
+        self.assertEqual(response.status_code, 200, response.text)
+        self.assertDictContainsSubset({
+            "iss": "http://localhost",
+            # "subject": "123456789012345",
+            "preferred_username": "james",
+            "given_name": "James",
+            "family_name": "Kirk",
+            "name": "James Kirk",
+            "gender": "male",
+            "birthdate": "1952-01-03",
+            "nickname": "Jim",
+            "phone_number": "+15182345678",
+            "email": "jamess@example.com",
+            "ial": '1',
+            # "id_assursance": [],
+            "document": [],
+            "address": []
+        }, response.json())
+
+    def test_update_user_success(self):
+        self.maxDiff = None
+        client = RequestsClient()
+        response = client.post(
+            "http://testserver/api/v1/user/",
+            json={
+                "preferred_username": "james",
+                "given_name": "James",
+                "family_name": "Kirk",
+                "gender": "M",
+                "password": "tree garden jump fox",
+                "birthdate": "1952-01-03",
+                "nickname": "Jim",
+                "phone_number": "+15182345678",
+                "email": "jamess@example.com",
+             })
+        update_response = client.put(
+             "http://testserver/api/v1/user/{}/".format(response.json()['sub']),
+             json={
+                "birthdate": "2233-03-22",
+                "family_name": "bob",
+             })
+        self.assertEqual(update_response.status_code, 200, update_response.text)
+        self.assertEqual("2233-03-22", update_response.json()['birthdate'])
+
+        up = UserProfile.objects.get(subject=response.json()['sub'])
+        self.assertEqual(up.birth_date,  datetime.date(2233, 3, 22))
+        self.assertEqual(up.user.last_name, "bob")
+
+    def test_delete_user_success(self):
+        self.maxDiff = None
+        client = RequestsClient()
+        response = client.post(
+            "http://testserver/api/v1/user/",
+            json={
+                "preferred_username": "james",
+                "given_name": "James",
+                "family_name": "Kirk",
+                "gender": "M",
+                "password": "tree garden jump fox",
+                "birthdate": "1952-01-03",
+                "nickname": "Jim",
+                "phone_number": "+15182345678",
+                "email": "jamess@example.com",
+             })
+        delete_response = client.delete(
+             "http://testserver/api/v1/user/{}/".format(response.json()['sub']))
+        self.assertEqual(delete_response.status_code, 204, delete_response.text)
+        with self.assertRaises(UserProfile.DoesNotExist):
+            UserProfile.objects.get(subject=response.json()['sub'])
--- apps/api/views/user.py
+++ apps/api/views/user.py
@@ -4,6 +4,7 @@ from apps.accounts.models import (
     UserProfile,
     GENDER_CHOICES,
 )
+from apps.oidc.claims import get_claims_provider
 # {
 #     "username": "james",
 #     "given_name": "James",
@@ -16,58 +17,63 @@ from apps.accounts.models import (
 #     "email": "jamess@example.com"
 # }
 User = get_user_model()
+ClaimsProvider = get_claims_provider()
 
 
 class UserSerializer(serializers.Serializer):
-    username = serializers.CharField(max_length=255, source='user.username')
+    preferred_username = serializers.CharField(max_length=255, source='user.username')
     given_name = serializers.CharField(max_length=255, source='user.first_name')
     family_name = serializers.CharField(max_length=255, source='user.last_name')
-    gender = serializers.ChoiceField(choices=GENDER_CHOICES)
+    gender = serializers.ChoiceField(choices=GENDER_CHOICES, source='sex')
     password = serializers.CharField(max_length=255, write_only=True, source='user.password')
-    birthdate = serializers.DateField()
+    birthdate = serializers.DateField(source='birth_date')
     nickname = serializers.CharField(max_length=255)
     email = serializers.EmailField(max_length=255, source='user.email')
-    phone_number = serializers.CharField(max_length=255)
+    phone_number = serializers.CharField(max_length=255, source='mobile_phone_number')
 
     def create(self, validated_data):
         # raise Exception(validated_data)
         user_data = validated_data.get('user', {})
         user = User.objects.create(**user_data)
 
-        return UserProfile.objects.create(
-            nickname=validated_data.get('nickname'),
-            gender_identity=validated_data.get('gender'),
-            mobile_phone_number=validated_data.get('phone_number'),
-            user=user,
-        )
+        validated_data['user'] = user
+        return UserProfile.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.nickname = validated_data.get('nickname')
-        instance.gender_identity = validated_data.get('gender')
-        instance.mobile_phone_number = validated_data.get('phone_number')
+        user_data = validated_data.pop('user', {})
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
         instance.save()
 
-        user_data = validated_data.get('user', {})
+        for attr, value in user_data.items():
+            setattr(instance.user, attr, value)
 
-        instance.user.username = user_data.get('requested_username')
-        instance.user.first_name = user_data.get('first_name')
-        instance.user.last_name = user_data.get('last_name')
-        instance.user.email = user_data.get('email')
         instance.user.save()
+
+        return instance
+
+    def to_representation(self, instance):
+        cp = ClaimsProvider(user=instance.user)
+        return cp.get_claims()
 
 
 class UserViewSet(viewsets.ModelViewSet):
-     lookup_field = "sub"
-     queryset = UserProfile.objects.all()
-     serializer_class = UserSerializer
+    lookup_field = "subject"
+    queryset = UserProfile.objects.all()
+    serializer_class = UserSerializer
 
-     # def create(self, request, *args, **kwargs):
-     #     pass
-     # def list(self, request, *args, **kwargs):
-     #     pass
-     # def retrieve(self, request, *args, **kwargs):
-     #     pass
-     # def update(self, request, *args, **kwargs):
-     #     pass
-     # def destroy(self, request, *args, **kwargs):
-     #     pass
+    def update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return super().update(request, *args, **kwargs)
+
+    # def create(self, request, *args, **kwargs):
+    #     pass
+    # def list(self, request, *args, **kwargs):
+    #     pass
+    # def retrieve(self, request, *args, **kwargs):
+    #     pass
+    # def update(self, request, *args, **kwargs):
+    #     pass
+    # def destroy(self, request, *args, **kwargs):
+    #     pass
